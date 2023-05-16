d812921137e163eb8edea00e21613085d9fefd2b
Zundullah BD <uluzundullah@gmail.com>
2023-05-16 23:42:15 +0700

    pre-deploy config

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ echo "Building the project..."
 python3 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+python3 manage.py makemigrations 
+python3 manage.py migrate 
 
 echo "Collect Static Files..."
 python3 manage.py collectstatic --noinput
