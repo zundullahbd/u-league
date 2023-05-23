a72a935f95ffc65bdccd3a66a8465e11853ddffd
fauziahpf <sissyfauziah@gmail.com>
2023-05-23 16:46:31 +0700

    continuing mengelola tim

--- manager/urls.py
+++ manager/urls.py
@@ -6,6 +6,7 @@ app_name = 'manager'
 
 urlpatterns = [
         path('', views.manager_home, name='manager_home'),
+        path('mengelola_tim/', views.mengelola_tim, name='mengelola_tim'),
         path('registrasi/', views.show_timregist, name='show_timregist'),
         path('detail/', views.show_teamdetail, name='show_teamdetail'),
         path('pemain/', views.show_addpemain, name='show_addpemain'),
